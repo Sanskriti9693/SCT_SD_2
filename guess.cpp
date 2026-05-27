03db69c40040b19f90bb93d5364b22b462bd7c03
sanskriti9693 <sanskriti@sanskritis-MacBook-Air.local>
2026-05-27 14:11:36 +0530

    Added Number Guessing Game project

---
+++ guess.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+    srand(time(0));
+
+    int randomNumber = rand() % 100 + 1;
+
+    int guess;
+
+    cout << "===== Number Guessing Game =====" << endl;
+    cout << "Guess a number between 1 and 100" << endl;
+
+    do {
+
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > randomNumber) {
+            cout << "Too High!" << endl;
+        }
+
+        else if (guess < randomNumber) {
+            cout << "Too Low!" << endl;
+        }
+
+        else {
+            cout << "Congratulations! You guessed the number." << endl;
+        }
+
+    } while (guess != randomNumber);
+
+    return 0;
+}
